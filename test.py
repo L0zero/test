cd5f50c67786d6844260d77ed9dd5f1eaab370c3
JiHub <JiHub@buaa.edu.cn>
2025-06-02 22:08:37 +0800

    refactor: replace test_func_123 with fib function

--- test.py
+++ test.py
@@ -1,9 +1,5 @@
-import numpy as np
-import pandas as pd
-
-def test_func_123(a, b):
-  return a+b
-
-if __name__ == '__main__':
-  print('hello')
-
+def fib(n):
+    if n <= 2:
+        return 1
+    else:
+        return fib(n-1) + fib(n-2)

217ca8307e306ae5038cbfa7588b227c6daea89a
JiHub <JiHub@buaa.edu.cn>
2025-06-03 00:01:16 +0800

    fix: update variable names for clarity

--- test.py
+++ test.py
@@ -1,10 +1,4 @@
-if __name__ == "__main__":  
-    n = int(input('fib: '))  
-    if a <= 2:
-        print(1)
-        return
- 
-    l, r = 1, 1
-    for i in range(a-2):
-        l, r = r, l + r
-    print(r)
+def fib(n):
+	if n <= 2:
+		return 1
+	return fib(n-1) + fib(n-2)

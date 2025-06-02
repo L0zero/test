b6303c036f57bb972ef59e22687c4d0cba2c43bb
JiHub <JiHub@buaa.edu.cn>
2025-06-02 22:51:25 +0800

    feat: refactor fib function to iterative approach

--- test.py
+++ test.py
@@ -1,5 +1,10 @@
-def fib(n):
-    if n <= 2:
-        return 1
-    else:
-        return fib(n-1) + fib(n-2)
+if __name__ == "__main__":  
+    n = int(input('fib: '))  
+    if a <= 2:
+        print(1)
+        return
+ 
+    l, r = 1, 1
+    for i in range(a-2):
+        l, r = r, l + r
+    print(r)

b89f9c0e0a75d2f0dc200f39680c7d39d6296245
JiHub <JiHub@buaa.edu.cn>
2025-06-03 10:29:27 +0800

    fix: add main block and correct fib function

--- test.py
+++ test.py
@@ -1,4 +1,8 @@
-def fib(n):
-	if n <= 2:
-		return 1
-	return fib(n-1) + fib(n-2)
+if __name__== "__main__":   
+    n = int(input('fib:'))
+    if n <= 2:
+        pirnt(1)
+        return
+    
+    l, r
+    
